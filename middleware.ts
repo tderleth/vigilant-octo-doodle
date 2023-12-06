6c7527f809bf1d3966feea5f7d0bb54bb5b357cb
Thomas Derleth <thomas.derleth@me.com>
2023-12-06 19:20:48 +0100

    added middleware and auth part

---
+++ middleware.ts
@@ -0,0 +1,39 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+type User = { username: string; password: string };
+
+const users: User[] = [
+  {
+    username: "user",
+    password: "password",
+  },
+];
+
+const findUserByUsername = (username: string): User | undefined => {
+  return users.find((user) => user.username === username);
+};
+
+export function middleware(req: NextRequest) {
+  const basicAuth = req.headers.get("authorization");
+  const url = req.nextUrl;
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(" ")[1];
+    const [username, pwd] = atob(authValue).split(":");
+
+    const user = findUserByUsername(username);
+
+    if (user && pwd === user.password) {
+      return NextResponse.next();
+    }
+  }
+
+  url.pathname = "/api/auth";
+
+  return NextResponse.rewrite(url);
+}
+
+export const config = {
+  matcher: ["/api/specs/:path*", "/", "/index"],
+};
